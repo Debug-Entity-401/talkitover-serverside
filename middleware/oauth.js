9a02f578a383cc7bc90ce5bac38d8f5b4a565871
Ammarhr <hariry49@gmail.com>
2020-06-21 03:43:16 +0300

    complete oauth for facbook

--- middleware/oauth.js
+++ middleware/oauth.js
@@ -1,85 +1,73 @@
 'use strict';
-
 const users = require('../src/users');
 const superagent = require('superagent');
-const signUp = require('../model/user-model');
 require('dotenv').config();
+const axios = require('axios').default;
 
-const CLIENT_ID = '58873f3b284a8f3adc6e';
-const CLIENT_SECRET = '5fd24e0c1639b375c2b4e379f1aa81646f8fa23e';
-const remoteUserApi = 'https://api.github.com/user';
-const API_SERVER = 'http://localhost:3031/oauth';
 
-const apiTokenUri = 'https://github.com/login/oauth/access_token';
+const remoteAPI = process.env.REMOTE_API;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const API_SERVER = process.env.API_SERVER; //local
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 module.exports = async function authorize(req, res, next) {
 
   try {
     let code = req.query.code;
-    console.log('(1) CODE:', code);
-
     let remoteToken = await exchangeCodeForToken(code);
-    console.log('(2) ACCESS TOKEN:', remoteToken);
-
-    let remoteUser = await getRemoteUserInfo(remoteToken);
-    console.log('(3) GITHUB USER', remoteUser);
-
+    let remoteUser = await getFacebookUserData(remoteToken);
     let [user, token] = await getUser(remoteUser);
     req.user = user;
     req.token = token;
-    console.log('(4) LOCAL USER', user);
 
     next();
-  } catch (e) { next(`ERROR: ${e.message}`);}
+  } catch (e) { next(`ERROR: ${e.message}`); }
 
 };
 
 async function exchangeCodeForToken(code) {
-  // console.log(CLIENT_ID);
-  // console.log(CLIENT_SECRET);
 
 
-
-  let tokenResponse = await superagent.post('https://developer19.us.auth0.com/oauth/token').send({
+  let tokenResponse = await superagent.post(API_SERVER).send({
     code: code,
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
-    redirect_uri: 'http://localhost:3031/oauth',
+    redirect_uri: REDIRECT_URI,
     grant_type: 'authorization_code',
   }).catch(e => console.log(e.message));
 
-
-
-  console.log(tokenResponse);
-
   let access_token = tokenResponse.body.access_token;
+  console.log(access_token);
 
   return access_token;
 
 }
 
-async function getRemoteUserInfo(token) {
-  // console.log(token);
-  let userResponse =
-        await superagent.get('https://developer19.us.auth0.com/userinfo')
-          .set('user-agent', 'express-app')
-          .set('Authorization', `Bearer ${token}`);
-  let user = userResponse.body;
-  // console.log('user res',user);
+// axios  send asynchronous HTTP request to REST endpoints and perform CRUD operations, (do superagent work with different features)
+async function getFacebookUserData(access_token) {
+  const { data } = await axios({
+    url: remoteAPI,
+    method: 'get',
+    params: {
+      fields: ['id', 'email', 'first_name', 'last_name'].join(','), // the data we request from FB API.
+      access_token: access_token,
+    },
+  });
 
-  return user;
-
+  return data;
 }
 
 async function getUser(remoteUser) {
   let userRecord = {
-    username: remoteUser.login,
+    user_name: `${remoteUser.first_name} ${remoteUser.last_name}`,
+    email: remoteUser.email,
     password: 'oauthpassword',
   };
 
-  let user = await users.save(userRecord);
-  let token = users.generateToken(user);
-  console.log('thisss==+++++++>', token);
+  let user = await users.saveHash(userRecord);
+  let token = users.getToken(user);
+  console.log(user);
 
   return [user, token];
 
