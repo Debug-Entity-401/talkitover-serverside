a2d171488c731bb23930ec0a0ad3fc20a7166cd3
sondoshassan <sondos199628@gmail.com>
2020-06-24 10:39:40 +0300

    clean code

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,7 @@
-// const io = require('socket.io-client');
-// const chat = io.connect('http://localhost:3030/chat');
-// 'http://localhost'
-// in index ejs <%# <a href="/< room>">chat</a> %> 
+
 console.log('hiiiiiiiiiiiiiiiiiii');
 const socket = io();
-// let messageForm = document.getElementById('chat-form');
-// const room = 'Private Chat Room';
-// console.log(client.request.headers.cookie);
+
 
 // socket.emit('joinRoom', room);  //3. send the room name to the server
 socket.on('joinRoom', room => {
--- src/routs/signout.js
+++ src/routs/signout.js
@@ -16,14 +16,11 @@ router.get('/signout', barearAuth, signoutHandler);
 function signoutHandler(req, res) {
   console.log('signed out!');
   if(req.headers.cookie) {
-    // console.log(req.headers.cookie);
     let token = req.headers.cookie.split('=');
-    // console.log(token);
     res.cookie('remember token', token, {
       expires: new Date(Date.now()),
       httpOnly: true,
     });
-    // console.log(res.cookie);
     res.redirect('/');
   }
 }
