a3155cb38aaa0c776fe1dbb535b3645eea6cd4b5
sondoshassan <sondos199628@gmail.com>
2020-07-24 14:28:59 +0300

    remove status

--- src/routs/signup.js
+++ src/routs/signup.js
@@ -32,7 +32,7 @@ function signUpUser(req, res, next) {
             expires: new Date(Date.now() + day),
             httpOnly: true,
           });
-          res.status(201).send(token);
+          res.send(token);
         }).catch(err => {
           res.status(403).send('Invalid Signup! email is taken');
         });
