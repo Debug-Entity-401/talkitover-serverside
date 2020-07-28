75ee2d1609596b9db9366db0fdb25469d061e9f4
Bushra-B <bushraxbil@gmail.com>
2020-07-28 10:23:24 +0300

    edited the articles schema, removed unnecessary

--- model/schema/articlesschema.js
+++ model/schema/articlesschema.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const articles = mongoose.Schema({
   title:{type:String,required:true},
   text:{type:String,required:true},
+  image: {type:String},
   status:{type:String,enum:['new','old'],required:true},
   url:{type:String,required:true},
   user:{type: mongoose.Schema.Types.ObjectId, ref:'user'},
