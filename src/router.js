3d34f6947d16a049929f266953cd0751da96f988
Ammarhr <hariry49@gmail.com>
2020-08-05 04:19:54 +0300

    new update for facebook

--- src/router.js
+++ src/router.js
@@ -1,40 +1,51 @@
-'use strict';
+"use strict";
 ////require
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const postmodule = require('../model/post-model');
-const User = require('../model/user-model');
-const Model = require('../model/general-model');
-const articles = new Model(require('../model/schema/articlesschema'));
-const bearerMiddleware = require('../middleware/bearer-auth');
-const aclMiddleware = require('../middleware/acl-middleware');
-const jwt = require('jsonwebtoken');
+const postmodule = require("../model/post-model");
+const User = require("../model/user-model");
+const Model = require("../model/general-model");
+const articles = new Model(require("../model/schema/articlesschema"));
+const bearerMiddleware = require("../middleware/bearer-auth");
+const aclMiddleware = require("../middleware/acl-middleware");
+const users = require('../src/users')
+const jwt = require("jsonwebtoken");
 ////////////////////
 //main, home, and profiles
-router.post('/assessment', registerHandler);
-router.get('/home', bearerMiddleware, homePageHandler);
-router.put('/profile/:id', editProfilePageHandler);
-router.get('/profile', bearerMiddleware, profilePageHandler);
-router.get('/otherprofile/:username', bearerMiddleware, otherUserProfileHandler);
+router.post("/assessment", registerHandler);
+router.get("/home", bearerMiddleware, homePageHandler);
+router.put("/profile/:id", editProfilePageHandler);
+router.get("/profile", bearerMiddleware, profilePageHandler);
+router.get(
+    "/otherprofile/:username",
+    bearerMiddleware,
+    otherUserProfileHandler,
+);
 //articles & quotes
 
-router.post('/user-articles/:idarticle', bearerMiddleware, addArticleUser);
-router.get('/user-articles', bearerMiddleware, readOne);
-router.delete('/user-articles/:idarticle', bearerMiddleware, deleteArticles);
+router.post("/user-articles/:idarticle", bearerMiddleware, addArticleUser);
+router.get("/user-articles", bearerMiddleware, readOne);
+router.delete("/user-articles/:idarticle", bearerMiddleware, deleteArticles);
 //posts
-router.get('/talkitoverposts', bearerMiddleware, postsHandler);
-router.post('/talkitoverposts', bearerMiddleware, aclMiddleware('POST'), addpostsHandler);
-router.put('/talkitoverposts/:idpost', bearerMiddleware, editpostsHandler);
-router.delete('/talkitoverposts/:idpost', bearerMiddleware, deletepostsHandler);
+router.get("/talkitoverposts", bearerMiddleware, postsHandler);
+router.post(
+    "/talkitoverposts",
+    bearerMiddleware,
+    aclMiddleware("POST"),
+    addpostsHandler,
+);
+router.put("/talkitoverposts/:idpost", bearerMiddleware, editpostsHandler);
+router.delete("/talkitoverposts/:idpost", bearerMiddleware, deletepostsHandler);
 
 ////////////////////
+router.post("/signinfp", signInWithFp);
 
 /**
- * 
+ *
  * @param {object} req
- * check if the req contains the user in the header 
- * @param {object} res 
+ * check if the req contains the user in the header
+ * @param {object} res
  * return the status of the user
  */
 function registerHandler(req, res) {
@@ -177,25 +188,25 @@ function registerHandler(req, res) {
         // score = 3 + Q2.indexOf(result.Question2) + Q3.indexOf(result.Question3) + Q4.indexOf(result.Question4) + Q5.indexOf(result.Question5);
 
         if (score < 5) {
-            status = 'good';
-            console.log('\n---------------------------\n score : ', score);
-            console.log(' status : ', status, '\n---------------------------\n');
-
+            status = "good";
+            console.log("\n---------------------------\n score : ", score);
+            console.log(" status : ", status, "\n---------------------------\n");
         } else if (score >= 5 && score <= 10) {
-            status = 'need help';
-            console.log('\n---------------------------\n score : ', score);
-            console.log(' status : ', status, '\n---------------------------\n');
-
+            status = "need help";
+            console.log("\n---------------------------\n score : ", score);
+            console.log(" status : ", status, "\n---------------------------\n");
         } else if (score > 10) {
-            status = 'extreme help';
-            console.log('\n---------------------------\n score : ', score);
-            console.log(' status : ', status, '\n---------------------------\n');
-
+            status = "extreme help";
+            console.log("\n---------------------------\n score : ", score);
+            console.log(" status : ", status, "\n---------------------------\n");
         }
         // })()
         //   .then((data) => {
         let username = req.body.token;
-        return jwt.verify(username, process.env.SECRET, async function(err, decoded) {
+        return jwt.verify(username, process.env.SECRET, async function(
+            err,
+            decoded,
+        ) {
             let decodedusername = decoded.user_name;
             User.assmentcreate(decodedusername, status);
             res.send({ user_name: decodedusername, status });
@@ -211,13 +222,12 @@ function registerHandler(req, res) {
     // }
 }
 
-
 /**
- * 
+ *
  * @param {object} req
- * get the user object from the request  
+ * get the user object from the request
  * @param {object} res
- * send a welcome message to the user 
+ * send a welcome message to the user
  */
 function homePageHandler(req, res) {
     const userInfo = req.user;
@@ -237,11 +247,28 @@ function homePageHandler(req, res) {
     res.status(200).send(userInfo.user_name);
 }
 
+async function signInWithFp(req, res) {
+    //         console.log('yaboooooyaaaa', req.body.user_name);
+    let dataRecord = await User.readUser(req.body.user_name);
+    console.log('useeeeeeeeeeer', dataRecord);
+    if (dataRecord[0]) {
+        let token = users.getToken(dataRecord[0])
+        let day = 86400000;
+        res.cookie('remember token', token, {
+            expires: new Date(Date.now() + day),
+            httpOnly: true,
+        });
+        console.log('this is the toooooooooooooken', token);
+        res.send(token);
+    } else {
+        res.send('invalid token');
+    }
+}
 /**
- * 
+ *
  * @param {Object} req
- * get the user object from the req 
- * @param {Object} res 
+ * get the user object from the req
+ * @param {Object} res
  * send the public user information from the database
  */
 
@@ -249,22 +276,21 @@ async function profilePageHandler(req, res) {
     //todo: show user's info in the profile page, including the articles and reviews (virtual joins)
     const user = await req.user;
     const username = await user.user_name;
-    await User.read(username)
-        .then(data => {
-            let userInfo = {
-                username: data.user_name,
-                photo: data.photo,
-                email: data.email,
-                country: data.country,
-                role: data.role,
-                status: data.status,
-                reviews: data.reviews,
-                articles: data.articles,
-                id: data._id,
-            };
-            //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-            res.status(200).send(JSON.stringify(userInfo));
-        });
+    await User.read(username).then((data) => {
+        let userInfo = {
+            username: data.user_name,
+            photo: data.photo,
+            email: data.email,
+            country: data.country,
+            role: data.role,
+            status: data.status,
+            reviews: data.reviews,
+            articles: data.articles,
+            id: data._id,
+        };
+        //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.status(200).send(JSON.stringify(userInfo));
+    });
 }
 async function editProfilePageHandler(req, res) {
     let id = req.params.id;
@@ -273,27 +299,32 @@ async function editProfilePageHandler(req, res) {
     let email = await req.body.email;
     let phonNumber = await req.body.phonNumber;
     let user_name = await req.body.user_name;
-    await User.updateProfile(id, Photo, country, email, phonNumber, user_name)
-        .then(data => {
-            res.status(200).json(data);
-        });
+    await User.updateProfile(
+        id,
+        Photo,
+        country,
+        email,
+        phonNumber,
+        user_name,
+    ).then((data) => {
+        res.status(200).json(data);
+    });
 }
 /**
- * 
+ *
  * @param {object} res
- *it will return all the posts 
+ *it will return all the posts
  */
 function postsHandler(req, res) {
-    postmodule.read()
-        .then(data => {
-            res.status(200).json(data);
-        });
+    postmodule.read().then((data) => {
+        res.status(200).json(data);
+    });
 }
 /**
- * 
- * @param {Object} req 
+ *
+ * @param {Object} req
  * will get the post information from the request body
- * @param {object} res 
+ * @param {object} res
  * it will return the created post
  */
 function addpostsHandler(req, res) {
@@ -301,143 +332,132 @@ function addpostsHandler(req, res) {
     const date = new Date(Date.now());
     let current_date = date.toDateString();
     newpost.date = current_date;
-    postmodule.create(newpost)
-        .then(data => {
-            res.status(201).json(data);
-        });
+    postmodule.create(newpost).then((data) => {
+        res.status(201).json(data);
+    });
 }
 /**
- * 
+ *
  * @param {object} req
  * it will get the username from the request object and the post if from the req param and get the post edits from the request body
- * @param {object} res 
+ * @param {object} res
  * it will send the updated post
  */
 function editpostsHandler(req, res) {
     let username = req.user.user_name;
     let idpost = req.params.idpost;
     let newpost = req.body;
-    User.read(username)
-        .then(data => {
-            postmodule.readById(idpost)
-                .then(postdata => {
-                    if (postdata[0].user_name === data.user_name) {
-                        postmodule.update(idpost, newpost)
-                            .then(data => {
-                                res.json(data);
-                            });
-                    } else {
-                        res.send('you connot update the post');
-                    }
+    User.read(username).then((data) => {
+        postmodule.readById(idpost).then((postdata) => {
+            if (postdata[0].user_name === data.user_name) {
+                postmodule.update(idpost, newpost).then((data) => {
+                    res.json(data);
                 });
+            } else {
+                res.send("you connot update the post");
+            }
         });
+    });
 }
 /**
- * 
+ *
  * @param {object} req
- * it will get the username from the request object and the post if from the req param 
- * @param {object} res 
+ * it will get the username from the request object and the post if from the req param
+ * @param {object} res
  * it will return a message if the post is deleted
  */
 function deletepostsHandler(req, res) {
     let username = req.user.user_name;
     let idpost = req.params.idpost;
-    User.read(username)
-        .then(data => {
-            postmodule.readById(idpost)
-                .then(postdata => {
-                    if (data.role === 'Administrators' || postdata[0].user_name === data.user_name) {
-                        postmodule.delete(idpost)
-                            .then(data => {
-                                res.send('post deleted');
-                            });
-                    } else {
-                        res.send('you connot delete');
-                    }
+    User.read(username).then((data) => {
+        postmodule.readById(idpost).then((postdata) => {
+            if (
+                data.role === "Administrators" ||
+                postdata[0].user_name === data.user_name
+            ) {
+                postmodule.delete(idpost).then((data) => {
+                    res.send("post deleted");
                 });
+            } else {
+                res.send("you connot delete");
+            }
         });
+    });
 }
 
-
 /**
- * 
+ *
  * @param {object} req
  * it will get the username from the request params and it will edit the user capablity in the request object
- * @param {object} res 
+ * @param {object} res
  * it will show the  public user profile
  */
 function otherUserProfileHandler(req, res) {
-    req.user.capabilities = ['READ', 'ADD REVIEW'];
+    req.user.capabilities = ["READ", "ADD REVIEW"];
     let username = req.params.username;
-    User.read(username)
-        .then(otherUser => {
-            if (otherUser && otherUser.user_name !== req.user.user_name) {
-                let otherUserInfo = {
-                    username: otherUser.user_name,
-                    photo: otherUser.photo,
-                    email: otherUser.email,
-                    country: otherUser.country,
-                    reviews: otherUser.reviews,
-                    role: otherUser.role,
-                    status: otherUser.status,
-                    id: otherUser._id,
-                };
-                res.status(200).send(JSON.stringify(otherUserInfo));
-            } else {
-                req.user.capabilities = ['READ', 'CREATE'];
-                //       res.redirect('/profile');
-            }
-        });
+    User.read(username).then((otherUser) => {
+        if (otherUser && otherUser.user_name !== req.user.user_name) {
+            let otherUserInfo = {
+                username: otherUser.user_name,
+                photo: otherUser.photo,
+                email: otherUser.email,
+                country: otherUser.country,
+                reviews: otherUser.reviews,
+                role: otherUser.role,
+                status: otherUser.status,
+                id: otherUser._id,
+            };
+            res.status(200).send(JSON.stringify(otherUserInfo));
+        } else {
+            req.user.capabilities = ["READ", "CREATE"];
+            //       res.redirect('/profile');
+        }
+    });
 }
 
 /**
- * 
- * @param {object} req 
+ *
+ * @param {object} req
  * it will get the username from the request object and get article id from request params
- * @param {object} res 
+ * @param {object} res
  * it will redirect to the profile page
  */
 function addArticleUser(req, res) {
     let id1 = req.user.user_name;
     let id2 = req.params.idarticle;
-    User.articleByUser(id1, id2)
-        .then(data => {
-            res.send(`Article ${id2} is Saved`);
-            // res.redirect('/profile');
-        });
+    User.articleByUser(id1, id2).then((data) => {
+        res.send(`Article ${id2} is Saved`);
+        // res.redirect('/profile');
+    });
 }
 
-
 /**
- * 
- * @param {Object} req 
- * it will get the username from the request object  
- * @param {Object} res 
- * it will return the user details including the saved article 
+ *
+ * @param {Object} req
+ * it will get the username from the request object
+ * @param {Object} res
+ * it will return the user details including the saved article
  */
-
 
 function readOne(req, res) {
     let userName = req.user.user_name;
-    User.read(userName)
-        .then(data => {
-            res.json(data);
-        });
+    User.read(userName).then((data) => {
+        res.json(data);
+    });
 }
 /**
- * 
- * @param {Object} req 
- * it will get the username from the request object and articleid from the params  
- * @param {Object} res 
+ *
+ * @param {Object} req
+ * it will get the username from the request object and articleid from the params
+ * @param {Object} res
  * it will redirect to the user profile
  */
 
 function deleteArticles(req, res) {
     let id1 = req.user.user_name;
     let id2 = req.params.idarticle;
-    User.deleteArticle(id1, id2)
-        .then(data => {
-            res.redirect('/profile');
-        });
+    User.deleteArticle(id1, id2).then((data) => {
+        res.redirect("/profile");
+    });
 }
 module.exports = router;
